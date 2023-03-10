1619106600318f40558164fa7137ebef875d0965
Chuanzheng Sun <48871022+Czshippee@users.noreply.github.com>
2023-03-10 06:34:16 -0800

    dinall3

---
+++ Video_Platform/src/test/java/com/example/Video_Platform/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Video_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
